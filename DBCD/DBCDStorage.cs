84724673d36db3d08e63301174742aa211132a4c
Barncastle <spearman.tom@gmail.com>
2019-06-12 10:55:03 +0100

    expose the backing data enforce readonly constraints on DBCDStorage

--- DBCD/DBCDStorage.cs
+++ DBCD/DBCDStorage.cs
@@ -1,6 +1,8 @@
 using DBCD.Helpers;
 using DBFileReaderLib;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
@@ -47,28 +49,34 @@ namespace DBCD
     public interface IDBCDStorage : IEnumerable<DynamicKeyValuePair<int>>, IDictionary<int, DBCDRow>
     {
         string[] AvailableColumns { get; }
+
+        /// <summary>
+        /// A readonly representation of the data as IDictionary&lt;int, <typeparamref name="T"/>&gt;
+        /// </summary>
+        IDictionary BackingCollection { get; }
     }
 
-    public class DBCDStorage<T> : Dictionary<int, DBCDRow>, IDBCDStorage where T : class, new()
+    public class DBCDStorage<T> : ReadOnlyDictionary<int, DBCDRow>, IDBCDStorage where T : class, new()
     {
         private readonly string[] availableColumns;
         private readonly string tableName;
         private readonly FieldAccessor fieldAccessor;
+        private readonly ReadOnlyDictionary<int, T> storage;
 
         string[] IDBCDStorage.AvailableColumns => this.availableColumns;
 
         public DBCDStorage(Stream stream, DBCDInfo info) : this(new DBReader(stream), info) { }
 
-        public DBCDStorage(DBReader dbReader, DBCDInfo info)
+        public DBCDStorage(DBReader dbReader, DBCDInfo info) : base(new Dictionary<int, DBCDRow>())
         {
             this.availableColumns = info.availableColumns;
             this.tableName = info.tableName;
             this.fieldAccessor = new FieldAccessor(typeof(T));
 
             // populate the collection so we don't iterate all values and create new rows each time
-            var records = dbReader.GetRecords<T>();
-            foreach (var record in records)
-                this.Add(record.Key, new DBCDRow(record.Key, record.Value, fieldAccessor));
+            storage = new ReadOnlyDictionary<int, T>(dbReader.GetRecords<T>());
+            foreach (var record in storage)
+                base.Dictionary.Add(record.Key, new DBCDRow(record.Key, record.Value, fieldAccessor));
         }
 
         IEnumerator<DynamicKeyValuePair<int>> IEnumerable<DynamicKeyValuePair<int>>.GetEnumerator()
@@ -77,6 +85,8 @@ namespace DBCD
             while (enumerator.MoveNext())
                 yield return new DynamicKeyValuePair<int>(enumerator.Current.Key, enumerator.Current.Value);
         }
+        
+        IDictionary IDBCDStorage.BackingCollection => new ReadOnlyDictionary<int, T>(storage);
 
         public override string ToString() => $"{this.tableName}";
 
