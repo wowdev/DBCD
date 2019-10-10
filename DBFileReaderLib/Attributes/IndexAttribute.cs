297992da28ae9a1fab13fe43e7f1cd1d4f18858f
Michał Kałużny <maku@justmaku.org>
2019-10-10 21:00:08 +0200

    Add inline field for the IndexAttribute

--- DBFileReaderLib/Attributes/IndexAttribute.cs
+++ DBFileReaderLib/Attributes/IndexAttribute.cs
@@ -4,5 +4,8 @@ namespace DBFileReaderLib.Attributes
 {
     public class IndexAttribute : Attribute
     {
+        public readonly bool Inline;
+
+        public IndexAttribute(bool inline) => Inline = inline;
     }
 }
