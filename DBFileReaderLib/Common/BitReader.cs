5695496d7174c53b0c660ec4563cbdde386feb41
Martin Benjamins <ictmartin@gmail.com>
2024-08-07 12:24:02 +0200

    Drop member prefixes in BitReader

--- DBFileReaderLib/Common/BitReader.cs
+++ DBFileReaderLib/Common/BitReader.cs
@@ -6,34 +6,32 @@ namespace DBFileReaderLib.Common
 {
     class BitReader
     {
-        private readonly byte[] m_array;
-        private int m_readPos;
-        private int m_readOffset;
+        private readonly byte[] Array;
 
-        public int Position { get => m_readPos; set => m_readPos = value; }
-        public int Offset { get => m_readOffset; set => m_readOffset = value; }
+        public int Position { get; set; }
+        public int Offset { get; set; }
 
-        public BitReader(byte[] data) => m_array = data;
+        public BitReader(byte[] data) => Array = data;
 
         public BitReader(byte[] data, int offset)
         {
-            m_array = data;
-            m_readOffset = offset;
+            Array = data;
+            Offset = offset;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public uint ReadUInt32(int numBits)
         {
-            uint result = Unsafe.As<byte, uint>(ref m_array[m_readOffset + (m_readPos >> 3)]) << (32 - numBits - (m_readPos & 7)) >> (32 - numBits);
-            m_readPos += numBits;
+            uint result = Unsafe.As<byte, uint>(ref Array[Offset + (Position >> 3)]) << (32 - numBits - (Position & 7)) >> (32 - numBits);
+            Position += numBits;
             return result;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ulong ReadUInt64(int numBits)
         {
-            ulong result = Unsafe.As<byte, ulong>(ref m_array[m_readOffset + (m_readPos >> 3)]) << (64 - numBits - (m_readPos & 7)) >> (64 - numBits);
-            m_readPos += numBits;
+            ulong result = Unsafe.As<byte, ulong>(ref Array[Offset + (Position >> 3)]) << (64 - numBits - (Position & 7)) >> (64 - numBits);
+            Position += numBits;
             return result;
         }
 
@@ -82,9 +80,9 @@ namespace DBFileReaderLib.Common
             unchecked
             {
                 int hash = 0;
-                for (int i = 0; i < m_array.Length; i++)
+                for (int i = 0; i < Array.Length; i++)
                 {
-                    hash += m_array[i];
+                    hash += Array[i];
                     hash += hash << 10;
                     hash ^= hash >> 6;
 
@@ -97,6 +95,6 @@ namespace DBFileReaderLib.Common
             }
         }
 
-        public BitReader Clone() => new BitReader(m_array);
+        public BitReader Clone() => new BitReader(Array);
     }
 }
